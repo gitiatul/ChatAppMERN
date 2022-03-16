f6c7cff762589ae32732ec3575d4ba82192706bb
gitiatul <atulbalodi6@gmail.com>
2022-03-16 11:54:02 +0530

    your commit message here

--- index.js
+++ index.js
@@ -28,16 +28,14 @@ app.use("/api/messages", messageRoutes);
 
 __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./public/build")));
+  app.use(express.static("public/build"));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "./public/build", "index.html"))
-  );
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "public", "build", "index.html"));
+  });
 }
-const PORT=process.env.PORT || 5000
-const server = app.listen(PORT, () =>
-  console.log(`Server started on ${PORT}`)
-);
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, () => console.log(`Server started on ${PORT}`));
 const io = socket(server, {
   cors: {
     origin: "http://localhost:3000",
