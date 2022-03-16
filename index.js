ed42cc40a89e31531e5bc2d689b3c2483fc1584d
gitiatul <atulbalodi6@gmail.com>
2022-03-16 12:16:22 +0530

    your commit message here

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => console.log(`Server started on ${PORT}`));
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     credentials: true,
   },
 });
